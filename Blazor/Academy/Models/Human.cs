df56401f9d43f411a7f15b8aae3a3d00f039d7e3
User <User@DESKTOP-QHG18FL>
2026-09-16 20:27:44 +0300

    Lesson 44 - TODO: Scaffold Student.

---
+++ Blazor/Academy/Models/Human.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Human
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]+$")]
+		public string last_name { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		public string first_name { get; set; }
+
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateOnly birth_date { get; set; }
+
+		[EmailAddress]
+		public string? email { get; set; }
+
+		[Phone]
+		public string? phone { get; set; }
+		public byte[]? photo { get; set; }
+	}
+}
---
+++ Blazor/Academy/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Student:Human
+	{
+		[Key]
+		public int stud_id { get; set; }
+
+		[Required]
+		public int group { get; set; }
+	}
+}
