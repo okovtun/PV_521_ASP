023f676be7023600f500af90838d4bf7dd1f0d1d
User <User@DESKTOP-QHG18FL>
2026-09-21 21:19:43 +0300

    Lesson 48 - Learning days in progress

---
+++ Blazor/Academy/Components/Pages/GroupPages/Week.cs
@@ -0,0 +1,21 @@
+﻿namespace Academy.Components.Pages.GroupPages
+{
+	public class Week
+	{
+		public static readonly string[] DAYNAMES = { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+		public int Days { get; set; }
+		public Week(int days)
+		{
+			this.Days = days;
+		}
+		public override string ToString()
+		{
+			List<string> result = new List<string>();
+			for (int i = 0; i < 7; i++)
+			{
+				if(((Days >> i) & 1) != 0)result.Add(DAYNAMES[i]);
+			}
+			return string.Join(", ", result);
+		}
+	}
+}
